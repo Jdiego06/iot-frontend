2a5682765e27f95a12ab91c06de9236349a844e2
Emanuel-Alzate-Aristizabal <emanuel960411@gmail.com>
2023-03-07 22:19:54 -0500

    Avances mostrando usuarios

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import {UserContextProvider} from './context/UserContext'
 
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
@@ -12,6 +13,8 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <App />
+    <UserContextProvider>
+      <App/>
+    </UserContextProvider>
   </React.StrictMode>
 );
